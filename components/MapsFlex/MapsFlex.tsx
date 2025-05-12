be98c730bf39add3c6e27e45b791f8e0443cbef3
finnharbeke <code@finnharbeke.ch>
2025-05-12 10:58:20 +0200

    MapsFlex component

---
+++ components/MapsFlex/MapsFlex.tsx
@@ -0,0 +1,27 @@
+import { Flex } from '@mantine/core';
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
+
+import classes from './MapsFlex.module.css';
+
+
+export function MapsFlex() {
+    const Map = useMemo(() => dynamic(
+        () => import('../Map/Map'),
+        { 
+            loading: () => <p>A map is loading</p>,
+            ssr: false
+        }
+        ), [])
+
+        
+    return <Flex className={classes.outer} gap="lg" >
+        <Flex className={classes.inner} direction={"column"} gap={"lg"}>
+            <Map position={[46.522, 6.633]} zoom={16} city='lausanne' />
+        </Flex>
+
+        <Flex className={classes.inner} direction={"column"} gap={"lg"}>
+            <Map position={[46.522, 6.633]} zoom={16} city='new york' />
+        </Flex>
+    </Flex>
+}
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,26 +1,5 @@
-import { Container, Flex, NativeSelect, Title } from '@mantine/core';
-// import { Map } from '../components/Map/Map';
-import { useMemo } from 'react';
-import dynamic from 'next/dynamic';
+import { MapsFlex } from '@/components/MapsFlex/MapsFlex';
 
 export default function MapPage() {
-    const Map = useMemo(() => dynamic(
-    () => import('../components/Map/Map'),
-    { 
-        loading: () => <p>A map is loading</p>,
-        ssr: false
-    }
-    ), [])
-
-      return (
-        <Flex direction={"row"} gap="lg" >
-            <Flex h={"750px"} w={"100%"} direction={"column"} gap={"lg"}>
-                <Map position={[46.522, 6.633]} zoom={16} city='lausanne' />
-            </Flex>
-
-            <Flex h={"750px"} w={"100%"} direction={"column"} gap={"lg"}>
-                <Map position={[46.522, 6.633]} zoom={16} city='new york' />
-            </Flex>
-        </Flex>
-    )
+    return <MapsFlex/>
 }
