f0728cad4c79fe830cc4e6a80ba319019e85ffc7
Finn Harbeke <code@finnharbeke.ch>
2026-07-07 15:39:39 +0000

    Change maxZoom to use curly braces in Map.tsx

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -44,7 +44,7 @@ export default function Map({ city_name }: {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
         subdomains='abcd'
-        maxZoom=20
+        maxZoom={20}
       />
       {
         layer != '' &&
