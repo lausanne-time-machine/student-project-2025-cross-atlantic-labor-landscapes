31eed96e741ce82c98a19ac2ba2f3412a507635b
Finn Harbeke <code@finnharbeke.ch>
2026-07-07 15:28:29 +0000

    base map tile layers to cartodb
    trying to make the base tile layer available, (stadia now has authentication)

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -41,8 +41,10 @@ export default function Map({ city_name }: {
     <MapContainer center={city.position} zoom={city.zoom} style={{ height: "100%", width: "100%" }}>
       {/* found here: https://leaflet-extras.github.io/leaflet-providers/preview/ */}
       <TileLayer
-        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+        subdomains='abcd'
+        maxZoom=20
       />
       {
         layer != '' &&
